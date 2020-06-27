d6097fb36de7e59304ac066d1b89112d65d6b16e
Usama <Usama18@icloud.com>
2020-06-28 02:16:08 +0500

    shifted to angular 10 and revisited all dependencies.

--- GreenClinic/ClientApp/src/app/shared/components/shared-components.module.ts
+++ GreenClinic/ClientApp/src/app/shared/components/shared-components.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PatientListComponent } from 'app/views/patients/patient-list/patient-list.component';
 import { ConfirmationModalComponent } from './confirmation-modal/confirmation-modal.component';
-import { MatInputModule, MatDialogModule } from '@angular/material';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
 import { RouterModule } from '@angular/router';
+import { AvatarModule } from 'ngx-avatar';
 
 const components =  [ConfirmationModalComponent];
 
@@ -10,7 +12,8 @@ const components =  [ConfirmationModalComponent];
     imports: [
         MatInputModule,
         RouterModule,
-        MatDialogModule
+        MatDialogModule,
+        AvatarModule
     ],
     declarations: components,
     exports: components
